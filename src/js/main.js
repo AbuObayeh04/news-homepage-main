df7a0f5b48972053c21d48618896f078b91a4c1d
Muhammad Abu Obayeh <mohamedaboobaya227@gmail.com>
2025-02-19 16:00:58 +0200

    Add files via upload

---
+++ src/js/main.js
@@ -0,0 +1,23 @@
+const landingPhoto = document.querySelector(".landing-photo");
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth < 768) {
+    landingPhoto.src = "../assets/images/image-web-3-mobile.jpg";
+  } else {
+    landingPhoto.src = "../assets/images/image-web-3-desktop.jpg";
+  }
+});
+
+const menuIcon = document.querySelector(".menu-icon");
+
+menuIcon.addEventListener("click", () => {
+  const menu = document.querySelector(".menu");
+  menu.classList.remove("hidden");
+});
+
+const iconClose = document.querySelector(".icon-close");
+
+iconClose.addEventListener("click", () => {
+  const menu = document.querySelector(".menu");
+  menu.classList.add("hidden");
+});
